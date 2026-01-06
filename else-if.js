84267295fb9b32c58980d922a394d0af8f3041df
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2026-01-06 21:50:31 +0600

    new problem solved

---
+++ else-if.js
@@ -0,0 +1,13 @@
+const isRain = false;
+if (isRain) {
+  console.log("Take an Umbrella");
+} else {
+  console.log("No Umbrella needed");
+}
+
+const price = 600;
+if (price >= 500) {
+  console.log("Delivery Charge Free");
+} else {
+  console.log("Give me delivery charge 50 tk only");
+}
