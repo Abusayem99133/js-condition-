e21ad5728606503667977e9e25ff74c262dce53c
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2026-01-06 21:37:23 +0600

    nested condition in practice

--- if-else.js
+++ if-else.js
@@ -1,6 +1,30 @@
 const mango = 34;
-if (mango > 30 || 40) {
+if (mango > 35) {
   console.log("now you  eating the mango ");
 } else {
   console.log("going to the home");
+}
+
+const age = 16;
+if (age >= 18) {
+  console.log("now you can vote");
+} else {
+  console.log("sorry, you cannot vote");
+}
+
+const marks = 55;
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 70) {
+  console.log("A-");
+} else if (marks >= 60) {
+  console.log("A");
+} else if (marks >= 50) {
+  console.log("B");
+} else if (marks >= 40) {
+  console.log("C");
+} else if (marks >= 33) {
+  console.log("D");
+} else {
+  console.log("Fail");
 }
