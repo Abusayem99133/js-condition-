4c0938280c2336b692b0e5c25c278c8ea33988c1
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2026-01-12 16:38:06 +0600

    new multiple conditon practice

---
+++ multiple.js
@@ -0,0 +1,9 @@
+const salary = 33000;
+const isGraduation = true;
+const isBcs = true;
+
+if ((salary >= 33500 && isGraduation) || isBcs) {
+  console.log("su.....patro");
+} else {
+  console.log("going to the another person");
+}
